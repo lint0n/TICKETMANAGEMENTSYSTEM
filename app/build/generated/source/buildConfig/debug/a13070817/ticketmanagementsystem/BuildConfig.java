1be7c9ef57d3bcc55128a77e08a8a9a17135cf04
Sam <s.linton93@gmail.com>
2017-05-02 02:09:55 +0100

    Project Final Version

---
+++ app/build/generated/source/buildConfig/debug/a13070817/ticketmanagementsystem/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package a13070817.ticketmanagementsystem;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "a13070817.ticketmanagementsystem";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
